1fbf19caef6f2feb167c6b5a323220197a9a4e2c
Alex Boulos <boulosalexandre@gmail.com>
2024-10-05 09:40:32 +0300

    show-client

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
 
+window.onload = function() {
+    const hash = window.location.hash;
+
+    // Check if the hash starts with #menuAbout
+    if (hash.startsWith('#aboutDropdown')) {
+        const param = hash.split(':')[1]; // Extract the parameter after the colon
+        if (param) {
+            aboutDropdown(parseInt(param)); // Pass the parameter to menuAbout, parsing it to an integer
+        }
+    }
+};
+
+
+document.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    const footer = document.getElementById('footer');
+    const rect = document.getElementById('rect');
+    const footerPosition = footer.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    // Check if footer is in view
+    if (footerPosition < screenHeight) {
+        navbar.classList.add('hidden');
+        rect.classList.add('hidden');
+    } else {
+        navbar.classList.remove('hidden');
+        rect.classList.remove('hidden');
+
+    }
+});
+
+
+
+// Function to handle menuAbout based on the parameter
+function menuAbout(about) {
+    switch (about) {
+        case 2:
+            aboutDropdown(2); // Call aboutDropdown function with parameter 2
+            break;
+        default:
+            console.log("No matching case for:", about);
+            break;
+    }
+}
+
+
+
 
 // Get the navbar element
 const navbar = document.getElementById("navbar");
 const recto = document.getElementById("rect");
+const logo = document.getElementById("logo");
 // navbar.classList.add("sticky");
 
 // Get the offset position of the navbar
@@ -13,10 +61,13 @@ function checkSticky() {
   if (window.scrollY >= 50) {
     navbar.classList.add("sticky");
     recto.classList.add("rectangle");
+    logo.classList.add('hidden');
 
   } else {
     navbar.classList.remove("sticky");
     recto.classList.remove("rectangle");
+    logo.classList.remove('hidden');
+
 
   }
 }
@@ -26,31 +77,6 @@ window.onscroll = function() {
   checkSticky();
 };
 
-
-//Slider 
-
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//     let i;
-//     let slides = document.getElementsByClassName("slide");
-//     let dots = document.getElementsByClassName("dot");
-//     if (n > slides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex-1].style.display = "block";
-//     dots[slideIndex-1].className += " active";
-// }
 
 
 function changeImage(imageSrc) {
@@ -69,6 +95,51 @@ function changeImage(imageSrc) {
     img.classList.add("slide-in");
 }
 
+
+function changeIndex(boxNumber) {
+    const indexImg = document.getElementById('index-header');
+    const box1 = document.getElementById('box1');
+    const box2 = document.getElementById('box2');
+    const box3 = document.getElementById('box3');
+
+
+    switch (boxNumber) {
+        case 1 :
+            indexImg.style.backgroundImage = "url('images/woman-with-food.png')";
+            box1.src = 'images/box-fill.png';
+            box2.src = 'images/box-empty.png';
+            box3.src = 'images/box-empty.png';
+            break;
+        case 2 :
+            indexImg.style.backgroundImage = "url('images/visual3.png')";
+            box1.src = 'images/box-empty.png';
+            box2.src = 'images/box-fill.png';
+            box3.src = 'images/box-empty.png';
+            break;
+        case 3 :
+            indexImg.style.backgroundImage = "url('images/woman-with-food.png')";
+            box1.src = 'images/box-empty.png';
+            box2.src = 'images/box-empty.png';
+            box3.src = 'images/box-fill.png';
+            break;
+    }
+}
+
+let into = 1;
+setInterval(changeContent(into++), 5000);
+
+
+
+// function changeSlider {
+//     const dots1 = document.querySelectorAll('.dot');
+//     if(dots1[0].style.backgroundColor === 'black'){
+//         changeContent(2);
+//     } else if(dots1[1].style.backgroundColor === 'black'){
+//         changeContent(3);
+//     } else if(dots1[2].style.backgroundColor === 'black'){
+//         changeContent(1);
+//     }
+// }
 
 function changeContent(dotNumber) {
     const dots = document.querySelectorAll('.dot');
@@ -113,6 +184,8 @@ function changeContent(dotNumber) {
     // Re-add the sliding animation class
     img.classList.add("love");
 }
+
+
 // Set initial state when page loads
 document.addEventListener('DOMContentLoaded', function() {
     changeContent(1); // This activates the first dot and sets the corresponding text and image.
@@ -123,26 +196,51 @@ function changeAbout(about) {
 
 }
 
-
-
 function aboutDropdown(about) {
     const aboutDescription = document.getElementById('about-description-text');
     const aboutNav = document.getElementById('about-nav-text');
+    const story = document.getElementById('story');
+    const mission = document.getElementById('mission');
+    const vision = document.getElementById('vision');
+    const quality = document.getElementById('quality');
+
 
     // Update text and image based on the selected dot
     switch (about) {
         case 1:
-            aboutDescription.innerHTML = "Adams Food Co. (The Real Lebanese Taste) started as a family business in 2010. Due to market need, pledged to create a homemade style frozen natural appetizers/finger food concept for consumers, prepared with quality ingredients. <br/> <br/> without any chemicals or artificial colorings for the whole family to enjoy morning, lunch, evening or snacking at all time, even for any type of event. <br/> <br/> Adams Food uses 100% natural fresh & quality ingredients for its prepared frozen products presented to you and your family especially children as a snack at home or to school to have and enjoy. <br/> <br/> Tradition and reliability sets Adams Food to the highest standards of frozen carefully prepared products and customer satisfaction."
-            aboutNav.innerHTML = "OUR STORY"
+            aboutDescription.innerHTML = "<b>Adams Food Co. (The Real Lebanese Taste) began as a family venture in 2010.</b><br/><br/>Recognizing a market need, we committed to crafting homemade-style frozen natural appetizers and finger foods. Our products are ade with high-quality ingredients, free from chemicals and artificial colorings, ensuring they are perfect for the whole family to enjoy at any time—be it morning, lunch, evening, or as a snack for any event.<br/><br/>At Adams Food, we pride ourselves on using 100% fresh, natural, and high-quality ingredients in our frozen products. These snacks are ideal for both home and school, especially for children to savor and enjoy.<br/><br/>Our dedication to tradition and reliability has established Adams Food as a leader in providing meticulously prepared frozen products, ensuring the highest standards of customer satisfaction.";
+            aboutNav.innerHTML = "OUR STORY";
+            story.style.color = '#ed7e2c';
+            mission.style.color = 'black';
+            vision.style.color = 'black';
+            quality.style.color = 'black';
             break;
         case 2:
-            aboutDescription.innerHTML = 'To reach every single home saving the hustle to consumers (Caterers, Hotels,  Restaurants & specially working wives) to prepare these products from scratch and have them baked/fried or cooked directly from the freezer. ';
-            aboutNav.innerHTML = "MISSION"
+            aboutDescription.innerHTML = 'To bring convenience to every home by providing high-quality, ready-to-cook frozen products. We aim to save time and effort for our consumers, including caterers, hotels, restaurants, and especially busy working wives, by offering delicious appetizers and finger foods that can be baked, fried, or cooked directly from the freezer.';
+            aboutNav.innerHTML = "MISSION";
+            story.style.color = 'black';
+            mission.style.color = '#ed7e2c';
+            vision.style.color = 'black';
+            quality.style.color = 'black';
             break;
         case 3:
-            aboutDescription.innerHTML = 'To be present all over the region being the reference of Lebanese quality frozen variety products providers from Kuwait for anyone who appreciates quality food. Adams Foods even custom make according to companies’ recipes maintaining the secrecy and confidentiality of their recipe. '
-            aboutNav.textContent = "VISION"
+            aboutDescription.innerHTML = 'To establish a strong presence across the region and globally as the leading provider of high-quality Lebanese frozen products from Kuwait.<br/><br/>We aim to be the go-to choice for anyone who values exceptional food. We are dedicated to maintaining the highest standards of quality in every product we offer, ensuring that our customers always receive the best.';
+            aboutNav.textContent = "VISION";
+            story.style.color = '#black';
+            mission.style.color = 'black';
+            vision.style.color = '#ed7e2c';
+            quality.style.color = 'black';
+            break;
+        case 4:
+            aboutDescription.innerHTML = 'To establish a strong presence across the region and globally as the leading provider of high-quality Lebanese frozen products from Kuwait.<br/><br/>We aim to be the go-to choice for anyone who values exceptional food. We are dedicated to maintaining the highest standards of quality in every product we offer, ensuring that our customers always receive the best.';
+            aboutNav.textContent = "QUALITY";
+            story.style.color = 'blacks';
+            mission.style.color = 'black';
+            vision.style.color = 'black';
+            quality.style.color = '#ed7e2c';
             break;
     }
     
 }
+
+
